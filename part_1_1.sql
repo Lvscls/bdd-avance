2d799692e34c55c19b81bc933f7548e70f7d20ea
Lilian <lilianvscls.pro@gmail.com>
2023-11-16 15:18:22 +0100

    Part 1_2

---
+++ part_1_1.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS "user" (
+    user_id SERIAL PRIMARY KEY,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    email VARCHAR(100),
+    username VARCHAR(50),
+    password VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS user_email_verification (
+    uev_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES "user"(user_id),
+    verified_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS session(
+    session_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES "user"(user_id),
+    connected_at TIMESTAMP
+);
+
+ALTER TABLE "user"
+ADD CONSTRAINT username_length CHECK (LENGTH(username) > 8);
+
+ALTER TABLE "user"
+ADD CONSTRAINT email_unique UNIQUE(email);
+
+ALTER TABLE "user"
+ADD CONSTRAINT password_length CHECK (LENGTH(password) > 8);
---
+++ part_1_2.py
@@ -0,0 +1,45 @@
+from faker import Faker
+import random
+import datetime
+
+fake = Faker()
+user_creation_dates = []
+email_verification_dates = []
+# Générer les requêtes INSERT INTO pour les 100 utilisateurs
+insert_user_queries = []
+for _ in range(100):
+    firstname = fake.first_name()
+    lastname = fake.last_name()
+    email = fake.email()
+    username = fake.user_name() + str(random.randint(100, 999))  # Assure l'unicité du nom d'utilisateur
+    password = fake.password(length=random.randint(8, 20))
+    created_at = fake.date_time_between_dates(datetime_start=datetime.datetime(2023, 1, 1),
+                                              datetime_end=datetime.datetime(2023, 6, 30))
+    user_creation_dates.append(created_at)
+    insert_query = f"INSERT INTO \"user\" (firstname, lastname, email, username, password, created_at) VALUES ('{firstname}', '{lastname}', '{email}', '{username}', '{password}', '{created_at}');"
+    insert_user_queries.append(insert_query)
+
+
+# Générer les requêtes INSERT INTO pour les 85 vérifications d'email
+insert_email_verification_queries = []
+for user_id in range(1, min(86, len(user_creation_dates) + 1)):
+    created_at = user_creation_dates[user_id - 1]
+    verified_at = fake.date_time_between_dates(datetime_start=created_at,
+                                               datetime_end=datetime.datetime(2023, 6, 30))
+    email_verification_dates.append(verified_at)
+    insert_query = f"INSERT INTO user_email_verification (user_id, verified_at) VALUES ({user_id}, '{verified_at}');"
+    insert_email_verification_queries.append(insert_query)
+
+
+
+insert_session_queries = []
+for _ in range(1000):
+    user_id = random.randint(1, 85)  # Choose a user with email verification
+    verification_date = email_verification_dates[user_id - 1]
+    connected_at = fake.date_time_between_dates(datetime_start=datetime.datetime(2023, 1, 1),
+                                                datetime_end=datetime.datetime(2023, 6, 30))
+    insert_query = f"INSERT INTO session (user_id, connected_at) VALUES ({user_id}, '{connected_at}');"
+    insert_session_queries.append(insert_query)
+
+with open('part_1_2.sql', 'w') as file:
+    file.write('\n'.join(insert_user_queries + insert_email_verification_queries + insert_session_queries))
