303c0a80296909d95f330d27803b5ecbf7aba8a4
Lilian <lilianvscls.pro@gmail.com>
2023-11-16 15:21:52 +0100

    FIX part1_2

--- part_1_2.py
+++ part_1_2.py
@@ -5,13 +5,13 @@ import datetime
 fake = Faker()
 user_creation_dates = []
 email_verification_dates = []
-# Générer les requêtes INSERT INTO pour les 100 utilisateurs
+
 insert_user_queries = []
 for _ in range(100):
     firstname = fake.first_name()
     lastname = fake.last_name()
     email = fake.email()
-    username = fake.user_name() + str(random.randint(100, 999))  # Assure l'unicité du nom d'utilisateur
+    username = fake.user_name() + str(random.randint(100, 999))  
     password = fake.password(length=random.randint(8, 20))
     created_at = fake.date_time_between_dates(datetime_start=datetime.datetime(2023, 1, 1),
                                               datetime_end=datetime.datetime(2023, 6, 30))
@@ -19,8 +19,6 @@ for _ in range(100):
     insert_query = f"INSERT INTO \"user\" (firstname, lastname, email, username, password, created_at) VALUES ('{firstname}', '{lastname}', '{email}', '{username}', '{password}', '{created_at}');"
     insert_user_queries.append(insert_query)
 
-
-# Générer les requêtes INSERT INTO pour les 85 vérifications d'email
 insert_email_verification_queries = []
 for user_id in range(1, min(86, len(user_creation_dates) + 1)):
     created_at = user_creation_dates[user_id - 1]
@@ -30,13 +28,12 @@ for user_id in range(1, min(86, len(user_creation_dates) + 1)):
     insert_query = f"INSERT INTO user_email_verification (user_id, verified_at) VALUES ({user_id}, '{verified_at}');"
     insert_email_verification_queries.append(insert_query)
 
-
-
 insert_session_queries = []
 for _ in range(1000):
-    user_id = random.randint(1, 85)  # Choose a user with email verification
+    user_id = random.randint(1, 85) 
     verification_date = email_verification_dates[user_id - 1]
-    connected_at = fake.date_time_between_dates(datetime_start=datetime.datetime(2023, 1, 1),
+    created_at = user_creation_dates[user_id - 1]
+    connected_at = fake.date_time_between_dates(datetime_start=max(created_at, verification_date),
                                                 datetime_end=datetime.datetime(2023, 6, 30))
     insert_query = f"INSERT INTO session (user_id, connected_at) VALUES ({user_id}, '{connected_at}');"
     insert_session_queries.append(insert_query)
